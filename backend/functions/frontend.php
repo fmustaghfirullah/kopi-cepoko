237290759d30d1ff69b3e1993bf9c12fe7b1f1ae
fmustaghfirullah <fmustaghfirullah@gmail.com>
2025-09-13 23:32:23 +0700

    admin fix

--- backend/functions/frontend.php
+++ backend/functions/frontend.php
@@ -1,6 +1,6 @@
 <?php
 // Frontend functions for Kopi Cepoko website
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../../config/database.php';
 
 
 class FrontendManager {
